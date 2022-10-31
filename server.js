95df5bac9f434d9b73159d9ffb4963d5fa9e2a27
Iosef14 <jeffersoncortez4@hotmail.com>
2022-10-30 19:31:47 -0600

    Ya estamos listos

--- server.js
+++ server.js
@@ -16,6 +16,12 @@ app.use(bodyParser.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
+app.get("/", (req, res) => {
+  res.json({ message: "HOLA Primera API" });
+});
+
+
 const db = require("./app/models");
 db.sequelize.sync();
 // // drop the table if it already exists
