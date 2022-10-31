9447fb1417db56acadf9c4af21a0b78afa0791fb
Iosef14 <jeffersoncortez4@hotmail.com>
2022-10-30 19:51:41 -0600

    Ya estamos listos

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 
 app.get("/", (req, res) => {
-  res.json({ message: "HOLA Primera API" });
+  res.json({ message: "HOLA Primera API sdasd" });
 });
 
 
